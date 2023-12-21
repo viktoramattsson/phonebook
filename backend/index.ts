e2a1c0b5cff977ce421eff71749308fd7af9da5d
Viktor Mattsson <viktor.a.mattsson@gmail.com>
2023-12-21 17:16:39 +0100

    setting up DB

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,42 @@
 import cors from 'cors'
+import * as dotenv from 'dotenv'
+import { Client } from 'pg'
 import express from 'express'
+
+dotenv.config()
+
+const client = new Client({
+  connectionString: process.env.PGURI
+})
+
+client.connect()
 
 const app = express()
 
 app.use(cors())
 
-app.get('/', (request, response) => {
-  response.send('Här kommer nummer visas')
+app.get('/', async (_request, response) => {
+  const { rows } = await client.query('SELECT * FROM numbers')
+
+  response.send(rows)
 })
 
 app.listen(3000, () => {
   console.log('Webbtjänsten kan nu ta emot anrop.')
 })
+
+
+// import cors from 'cors'
+// import express from 'express'
+
+// const app = express()
+
+// app.use(cors())
+
+// app.get('/', (request, response) => {
+//   response.send('Här kommer nummer visas')
+// })
+
+// app.listen(3000, () => {
+//   console.log('Webbtjänsten kan nu ta emot anrop.')
+// })
--- frontend/src/Components/Phonebookmodal.tsx
+++ frontend/src/Components/Phonebookmodal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 
 import './modal.css';
 
@@ -6,17 +6,20 @@ interface PhonebookProps {
   onClose: () => void;
 }
 
+interface numbers {
+  id: number,
+  name: string,
+  number: string
+}
 
-
-function PhonebookModal({ onClose }: PhonebookProps) {
-
-  const [data, setData] = useState<string | null>(null)
+function PhonebookModal({ onClose }: PhonebookProps): JSX.Element {
+  const [data, setData] = useState<numbers[] | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:3000');
-        const result = await response.text();
+        const result: numbers[] = await response.json();
         setData(result);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -29,7 +32,7 @@ function PhonebookModal({ onClose }: PhonebookProps) {
   return (
     <div className="modal">
       <h2>Phonebook modal!</h2>
-      <p>{data || 'Loading...'}</p>
+      <p>{data && data.length>0 && data[0].name}</p>
       <button onClick={onClose}>Close</button>
     </div>
   );
